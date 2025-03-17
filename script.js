857b6a89dfdfeafe8a67fe3644fb9ff0bcdb09a1
Eddie <Eddie11910@outlook.com>
2025-03-16 22:54:30 -0700

    Implemented the gameboard object

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-const Gameboard = (function()
+const gameboard = (function()
 {
-    
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const getBoard = () => board;
+
+    const updateBoard = (index, character) => board.at(index) = character; 
+
+    const resetBoard = () => board.fill("");
+
+    return {getBoard, updateBoard, resetBoard};
+})();
+
+const player = function()
+{
+    return {};
+};
+
+const displayController = (function()
+{
+
+    return {};
+});
+
+const gameController = (function()
+{
+    return {};
 })();
